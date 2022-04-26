07b35b997e229846311cb5722c49bf4a4834f53f
Saim Raza <saimorsaim@gmail.com>
2022-04-25 22:27:50 -0400

    Improve rename_files to not overwrite existing images

--- rename_files.py
+++ rename_files.py
@@ -3,12 +3,21 @@
 import os
 from sys import argv
 
-dir = argv[1]
+dir = os.path.realpath(argv[1])
 os.chdir(dir)
 print(f"Renaming files in {dir}")
 
-for count, f in enumerate(os.listdir()):
-	f_name, f_ext = os.path.splitext(f)
-	f_name = f"img{count:02d}"
-	new_name = f'{f_name}{f_ext}'
-	os.rename(f, new_name)
+# Save file contents with name in order to avoid 
+# overwriting of existing files
+new_files = {}
+
+for count, filename in enumerate(os.listdir()):
+    f_name, f_ext = os.path.splitext(filename)
+    new_name = f'{1 + count:03d}{f_ext}'
+    with open(filename, "rb") as f:
+        new_files[new_name] = f.read()
+    os.unlink(filename)
+
+for new_name, f_data in new_files.items():
+    with open(new_name, "wb+") as f:
+        f.write(f_data)
