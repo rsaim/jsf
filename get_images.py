2c0ab73877b9394273482b111b2139f344bdc30d
Saim Raza <saimorsaim@gmail.com>
2022-04-25 22:43:00 -0400

    Add a wrapper script to download images from a URL to a directory

---
+++ get_images.py
@@ -0,0 +1,12 @@
+from subprocess import Popen
+from sys import argv
+import os
+
+url = argv[1]
+dir = os.path.realpath(argv[2])
+os.chdir(dir)
+print(f"Downloading images from {url} to {dir}")
+
+Popen(f"wget -p -A .jpg,.jpeg,.png -H -nd {url}", shell=True).wait()
+Popen(f"python3 ~/github/jsf/remove_similar_images.py .", shell=True).wait()
+Popen(f"python3 ~/github/jsf/rename_files.py .", shell=True).wait()
